85181fd6d4bd31f75152dc166ac60b4ada5a0c69
matthew-lean <matt.1001@live.co.uk>
2017-03-23 17:45:05 +0000

    new thin layout and minimalised the blog post excerpt

--- Blog.php
+++ Blog.php
@@ -6,6 +6,12 @@
 
 ?>
 
+<div class="banner">
+		<svg xmlns="http://www.w3.org/2000/svg" width="70px" height="auto" viewBox="0 0 476.3 437.8" style="enable-background:new 0 0 476.3 437.8" xml:space="preserve">
+				<polyline class="white" points="239.1,290.6 343.1,186.7 346,183.7 346,437.8 476.3,413.1 476.3,24.7 346,0 238.7,107 238.7,107 130.3,0 0,24.7 0,413.1 130.3,437.8 130.3,182.8 130.3,182.8 239.1,290.6 331.3,198.5 346,183.7"></polyline>
+		</svg>
+</div>
+
 <?php get_header(); ?>
 
 	<?php query_posts("posts_per_page=25")?>
--- Home.php
+++ Home.php
@@ -7,7 +7,7 @@
 ?>
 
 <?php get_header(); ?>
-
+				<div class="home">
 	        <div class="primary">
 	            <div class="content-wrap">
 	                <div class="content">
@@ -111,11 +111,6 @@
 			<?php endwhile; ?>
 			</div>
 
-			<div class="banner">
-	        <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="auto" viewBox="0 0 476.3 437.8" style="enable-background:new 0 0 476.3 437.8" xml:space="preserve">
-	            <polyline class="white" points="239.1,290.6 343.1,186.7 346,183.7 346,437.8 476.3,413.1 476.3,24.7 346,0 238.7,107 238.7,107 130.3,0 0,24.7 0,413.1 130.3,437.8 130.3,182.8 130.3,182.8 239.1,290.6 331.3,198.5 346,183.7"></polyline>
-	        </svg>
-	    </div>
 	    <div class="tertiary">
 	        <div class="main-title">
 	            <h3>Graphic Design Work</h3>
@@ -163,6 +158,7 @@
 	            </div>
 	        </div>
 	    </div>
+		</div>
 
 	<?php else : ?>
 
