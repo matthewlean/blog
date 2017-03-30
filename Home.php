762f142b44a176c1ee8177058b13f10a1858c60b
matthew-lean <matt.1001@live.co.uk>
2017-03-30 09:18:30 +0100

    finished blog page

--- Home.php
+++ Home.php
@@ -64,18 +64,23 @@
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-			<a href="<?php the_permalink(); ?>">
-				<div <?php post_class('project-list-item ms-item col-lg-6 col-md-6 col-sm-6 col-xs-12') ?>">
-							<div class="content">
-								<div class="content-wrap">
-	        					<div class="title">
-		        					<h3> <?php the_title(); ?></h3>
-	        					</div>
-
+				<a href="<?php the_permalink(); ?>">
+					<div <?php post_class('project-list-item ms-item col-lg-6 col-md-6 col-sm-6 col-xs-12') ?>">
+								<div class="content">
+									<div class="thumb">
+										<?php the_post_thumbnail('small'); ?>
+									</div>
+									<div class="content-wrap">
+		        					<div class="title">
+			        					<h3> <?php the_title(); ?></h3>
+		        					</div>
+							         <div class="description">
+							         	<?php the_excerpt(); ?>
+							         </div>
+									</div>
 								</div>
 							</div>
-						</div>
-			</a>
+				</a>
 		<?php endwhile; ?>
 	</div>
 
